5fafbee39ae8d0984ce38ecfcc939ddef6f6935e
bababouf <Anthony.Gregor01@student.csulb.edu>
2024-04-15 14:20:22 -0700

    Deleted duplicate files. Replaced SQLite server with MYSQL server. Updated POM.xml to reflect DB change. Implemented basic servlet that is invoked when user enters a query (communication is established, but currently nothing is sent back to client).

---
+++ src/main/java/servlets/ServletUtilities.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ServletUtilities {
+
+    public static JsonObject parseRequestBody(BufferedReader reader) throws IOException {
+        StringBuilder requestBody = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            requestBody.append(line);
+        }
+        return JsonParser.parseString(requestBody.toString()).getAsJsonObject();
+    }
+
+}
